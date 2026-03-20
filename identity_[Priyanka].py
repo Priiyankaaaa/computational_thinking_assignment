90c98f41d4f0f438790189e094e4202ab13cb4dc
Priiyankaaaa <priyanka.jadhav@avantika.edu.in>
2026-03-20 06:39:19 +0000

    Version 1

--- identity_[Priyanka].py
+++ identity_[Priyanka].py
@@ -1,1 +1,31 @@
+# Task 1: Identity Investigation
+#Integers
+a = 10
+print("Before: ", id(a))
+a = 20
+print("After:", id(a))
+
+#String
+s = "Hii"
+print("\nBefore: ", id(s))
+s = "Hello"
+print("After:", id(s))
+
+#List
+l = [1, 2, 3]
+print("\nBefore: ", id(l))
+l.append(4)
+print("After:", id(l))
+
+#Tuple
+t = (1, 2, 3)
+print("\nBefore: ", id(t))
+t = (4, 5, 6)
+print("After:", id(t))
+
+#Float
+f = 3.14
+print("\nBefore: ", id(f))
+f = 4.1
+print("After: ", id(f))
 
